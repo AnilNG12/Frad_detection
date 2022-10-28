19e3c9bfa57e0530313837ce7303db52e8744289
AnilNG12 <anilkumar.ng1@gmail.com>
2022-10-28 21:02:45 +0530

    testing revert for sum function

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,3 @@
 print("adding al lthe data processing code related to frad detection")
 
-#testing revert 
-a = 4
-b = 5
-def sum(a,b):
-    c=a+b
-    return(a)
-print("testing wrong code for sum ")
+test
